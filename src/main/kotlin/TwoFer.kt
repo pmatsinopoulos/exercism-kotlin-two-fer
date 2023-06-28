c5d06d5d365ac788a5e4e90aed79c5be6dd05058
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-28 08:39:17 +0300

    Simplified by setting the default value to 'you'

--- src/main/kotlin/TwoFer.kt
+++ src/main/kotlin/TwoFer.kt
@@ -1,4 +1,1 @@
-fun twofer(name: String? = null) = when (name) {
-    null -> "One for you, one for me."
-    else -> "One for $name, one for me."
-}
+fun twofer(name: String? = "you") = "One for $name, one for me."
