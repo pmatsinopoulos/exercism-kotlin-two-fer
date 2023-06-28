e7832b147cded24e49da333763fdf81bd6d75b98
Panos Matsinopoulos <panos@mixlr.com>
2023-06-28 08:33:15 +0300

    Implemented the necessary function

--- src/main/kotlin/TwoFer.kt
+++ src/main/kotlin/TwoFer.kt
@@ -1,3 +1,4 @@
-fun twofer(name: String): String {
-    TODO("Implement the function to complete the task")
+fun twofer(name: String? = null) = when (name) {
+    null -> "One for you, one for me."
+    else -> "One for $name, one for me."
 }
